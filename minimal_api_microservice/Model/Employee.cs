6d9d9ae7b2216fc0dc33fb937b862979ad51c367
Abhishek Raj <abhishekraj1130@gmail.com>
2022-05-25 09:56:41 +0530

    model check

---
+++ minimal_api_microservice/Model/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace minimal_api_microservice.Model
+{
+    public class Employee
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string location { get; set; }
+    }
+}
--- minimal_api_microservice/Program.cs
+++ minimal_api_microservice/Program.cs
@@ -1,6 +1,16 @@
+using minimal_api_microservice.Model;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/employee", (Func<Employee>)(() => {
+    return new Employee()
+    {
+        Name = "Abhishek",
+        location = "India",
+        Id = "1"
+    };
+}));
 
 app.Run();
