c8a6fe6861a71ff242289454101d9b67812bb2f3
Abhishek Raj <abhishekraj1130@gmail.com>
2022-05-29 19:32:34 +0530

    Db connected to SQL and created schema by ef

--- minimal_api_microservice/Program.cs
+++ minimal_api_microservice/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
 using minimal_api_microservice.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+var connectionString = builder.Configuration.GetConnectionString("AppDb");
+builder.Services.AddDbContext<EmployeeDbContext>(x => x.UseSqlServer(connectionString));
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/employee", (Func<Employee>)(() =>
@@ -14,23 +18,23 @@ app.MapGet("/employee", (Func<Employee>)(() =>
     };
 }));
 
-app.MapGet("/employees", (Func<List<Employee>>)(() =>
-{
-    return new EmployeeCollection().GetEmployees();
-}));
+//app.MapGet("/employees", (Func<List<Employee>>)(() =>
+//{
+//    return new EmployeeCollection().GetEmployees();
+//}));
 
-app.MapGet("/employee/{id}", async (http) =>
-{
-    if (!http.Request.RouteValues.TryGetValue("id", out var id))
-    {
-        http.Response.StatusCode = 400;
-    }
-    else
-    {
-        await http.Response.WriteAsJsonAsync(new EmployeeCollection()
-            .GetEmployees()
-            .FirstOrDefault(x => x.Id == (string)id));
-    }
-});
+//app.MapGet("/employee/{id}", async (http) =>
+//{
+//    if (!http.Request.RouteValues.TryGetValue("id", out var id))
+//    {
+//        http.Response.StatusCode = 400;
+//    }
+//    else
+//    {
+//        await http.Response.WriteAsJsonAsync(new EmployeeCollection()
+//            .GetEmployees()
+//            .FirstOrDefault(x => x.Id == (string)id));
+//    }
+//});
 
 app.Run();
